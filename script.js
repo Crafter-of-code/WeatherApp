6bcba81bf55c7ad734ab6e1282ea55a75fb6d400
Uzair <mduzairkhan4@gmail.com>
2025-01-24 02:12:04 +0530

    The designing of the website had been and external api also been handel

--- script.js
+++ script.js
@@ -1,28 +1,51 @@
-// document.querySelector(".Heading").innerHTML = "hello world";
-let input_from_the_user = document.querySelector(".user_input_field");
-document
-  .querySelector(".user_input_button")
-  .addEventListener("click", (event) => {
-    event.preventDefault();
-    console.log(input_from_the_user.value);
-    input_from_the_user.value = null;
-  });
-document.querySelector(".Location").innerHTML = "Delhi";
-document.querySelector(".Temperature").innerHTML = "32";
-let air_quality = 1;
-let humidity = 2;
-let wind = 3;
-let presipitation = 4;
-let visibility = 5;
-document.querySelector(".air_quality").innerHTML = `AQI: ${air_quality}`;
-document.querySelector(".humidity").innerHTML = `Humidity ${humidity}`;
-document.querySelector(".wind").innerHTML = `Wind: ${wind}`;
-document.querySelector(
-  ".presipitation"
-).innerHTML = `Presipitation: ${presipitation}`;
-document.querySelector(".visibility").innerHTML = `Visibility ${visibility}`;
-// Air;
-// Humidity;
-// wind;
-// Presipitation;
-// visibility;
+const Temperature = document.querySelector(".Temperature_input");
+const Location = document.querySelector(".Location_input");
+const Humidity = document.querySelector("#Humidity_input");
+const Visility = document.querySelector("#Visility_input");
+const Precipitation = document.querySelector("#Precipitation_input");
+const Wind_speed = document.querySelector("#Wind_speed_input");
+const content_page = document.querySelector(".content_page");
+
+// const input_box_2 = document.querySelector("#input_box_2");
+let value_of_first_input;
+let response;
+async function first_page_click_handler() {
+  if (input_box_1 == "") {
+    console.log("the value is empty");
+    return;
+  }
+  // await fetch(
+  //   `https://api.tomorrow.io/v4/weather/realtime?location=${input_box_2.value}&apikey=Y0S2O1N9UqRKRTUjsDkS2AzvpMgvflnB`
+  // )
+  //   .then((res) => res.json())
+  //   .then((res) => (response = res))
+  //   .catch((err) => console.error(err));
+
+  console.log(input_box_1.value);
+  // content_page.scrollIntoView();
+
+  // setTimeout(() => {
+  //   document
+  //     .querySelector(".landing_page")
+  //     .style.setProperty("display", "none");
+  // }, 1000);
+  // console.log(first_input_box.current.value);
+  // console.log(value_of_first_input);
+  console.log(response);
+  input_box_1.value = "";
+}
+async function content_page_click_handler() {
+  await fetch(
+    `https://api.tomorrow.io/v4/weather/realtime?location=${input_box_2.value}&apikey=Y0S2O1N9UqRKRTUjsDkS2AzvpMgvflnB`
+  )
+    .then((res) => res.json())
+    .then((res) => (response = res))
+    .catch((err) => console.error(err));
+  input_box_2.value = "";
+}
+Temperature.innerText = response.data.values.temperature;
+Location.innerText = response.location.name;
+Humidity.innerText = response.data.values.humidity;
+Visility = response.data.values.visibility;
+Precipitation = response.data.values.precipitation;
+Wind_speed = response.data.values.windSpeed;
