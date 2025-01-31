6189a5775524644e89d9b01fea3a6492d6612c7f
Uzair <mduzairkhan4@gmail.com>
2025-01-31 14:37:12 +0530

    api calls were not working properly so some changes has been made

--- script.js
+++ script.js
@@ -1,51 +1,59 @@
 const Temperature = document.querySelector(".Temperature_input");
 const Location = document.querySelector(".Location_input");
 const Humidity = document.querySelector("#Humidity_input");
-const Visility = document.querySelector("#Visility_input");
+const Visility = document.querySelector("#Visibility_input");
 const Precipitation = document.querySelector("#Precipitation_input");
 const Wind_speed = document.querySelector("#Wind_speed_input");
 const content_page = document.querySelector(".content_page");
 
 // const input_box_2 = document.querySelector("#input_box_2");
 let value_of_first_input;
-let response;
 async function first_page_click_handler() {
+  let response;
   if (input_box_1 == "") {
     console.log("the value is empty");
     return;
   }
-  // await fetch(
-  //   `https://api.tomorrow.io/v4/weather/realtime?location=${input_box_2.value}&apikey=Y0S2O1N9UqRKRTUjsDkS2AzvpMgvflnB`
-  // )
-  //   .then((res) => res.json())
-  //   .then((res) => (response = res))
-  //   .catch((err) => console.error(err));
-
-  console.log(input_box_1.value);
-  // content_page.scrollIntoView();
-
-  // setTimeout(() => {
-  //   document
-  //     .querySelector(".landing_page")
-  //     .style.setProperty("display", "none");
-  // }, 1000);
-  // console.log(first_input_box.current.value);
-  // console.log(value_of_first_input);
-  console.log(response);
-  input_box_1.value = "";
-}
-async function content_page_click_handler() {
   await fetch(
-    `https://api.tomorrow.io/v4/weather/realtime?location=${input_box_2.value}&apikey=Y0S2O1N9UqRKRTUjsDkS2AzvpMgvflnB`
+    `https://api.tomorrow.io/v4/weather/realtime?location=${input_box_1.value}&apikey=Y0S2O1N9UqRKRTUjsDkS2AzvpMgvflnB`
   )
     .then((res) => res.json())
     .then((res) => (response = res))
     .catch((err) => console.error(err));
-  input_box_2.value = "";
+
+  console.log(input_box_1.value);
+
+  console.log(response);
+  input_box_1.value = "";
+  if (response.ok) {
+    content_page.scrollIntoView();
+    setTimeout(() => {
+      document
+        .querySelector(".landing_page")
+        .style.setProperty("display", "none");
+    }, 1000);
+    Temperature.innerText = response.data.values.temperature;
+    Location.innerText = response.location.name;
+    Humidity.innerText = response.data.values.humidity;
+    Visility.innerText = response.data.values.visibility;
+    Precipitation.innerText = response.data.values.precipitationProbability;
+    Wind_speed.innerText = response.data.values.windSpeed;
+  } else {
+    console.log("response is not okay");
+  }
 }
-Temperature.innerText = response.data.values.temperature;
-Location.innerText = response.location.name;
-Humidity.innerText = response.data.values.humidity;
-Visility = response.data.values.visibility;
-Precipitation = response.data.values.precipitation;
-Wind_speed = response.data.values.windSpeed;
+// async function content_page_click_handler() {
+//   await fetch(
+//     `https://api.tomorrow.io/v4/weather/realtime?location=${input_box_2.value}&apikey=Y0S2O1N9UqRKRTUjsDkS2AzvpMgvflnB`
+//   )
+//     .then((res) => res.json())
+//     .then((res) => (response = res))
+//     .catch((err) => console.error(err));
+//   input_box_2.value = "";
+// }
+// Temperature.innerText = response.data.values.temperature;
+// Location.innerText = response.location.name;
+// Humidity.innerText = response.data.values.humidity;
+// Visility = response.data.values.visibility;
+// Precipitation = response.data.values.precipitation;
+// Wind_speed = response.data.values.windSpeed;
